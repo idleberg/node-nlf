9ccc3dc749276d42fe9a6fc710cf5a6e8ad596ec
idleberg <jan@idleberg.com>
2018-08-15 16:37:35 +0200

    update tests

--- test/default.js
+++ test/default.js
@@ -8,8 +8,16 @@ const arabicFile = readFileSync(join(__dirname, '/fixtures/Arabic.nlf'), 'utf8')
 const arabicJSON = readFileSync(join(__dirname, '/fixtures/Arabic.json'), 'utf8');
 const englishFile = readFileSync(join(__dirname, '/fixtures/English.nlf'), 'utf8');
 const englishJSON = readFileSync(join(__dirname, '/fixtures/English.json'), 'utf8');
+const farsiFile = readFileSync(join(__dirname, '/fixtures/Farsi.nlf'), 'utf8');
+const farsiJSON = readFileSync(join(__dirname, '/fixtures/Farsi.json'), 'utf8');
+const greekFile = readFileSync(join(__dirname, '/fixtures/Greek.nlf'), 'utf8');
+const greekJSON = readFileSync(join(__dirname, '/fixtures/Greek.json'), 'utf8');
+const koreanFile = readFileSync(join(__dirname, '/fixtures/Korean.nlf'), 'utf8');
+const koreanJSON = readFileSync(join(__dirname, '/fixtures/Korean.json'), 'utf8');
 const japaneseFile = readFileSync(join(__dirname, '/fixtures/Japanese.nlf'), 'utf8');
 const japaneseJSON = readFileSync(join(__dirname, '/fixtures/Japanese.json'), 'utf8');
+const thaiFile = readFileSync(join(__dirname, '/fixtures/Thai.nlf'), 'utf8');
+const thaiJSON = readFileSync(join(__dirname, '/fixtures/Thai.json'), 'utf8');
 
 // Let's run the tests
 test('Arabic: Convert', t => {
@@ -29,6 +37,69 @@ test('Arabic: Parse', t => {
 test('Arabic: Stringify', t => {
   const actual = NLF.stringify(JSON.parse(arabicJSON), false);
   const expected = arabicFile;
+
+  t.is(actual.trim(), expected.trim());
+});
+
+test('English: Convert', t => {
+  const expected = englishFile;
+  const actual = NLF.stringify(NLF.parse(expected));
+
+  t.is(actual.trim(), expected.trim());
+});
+
+test('English: Parse', t => {
+  const actual = NLF.parse(englishFile);
+  const expected = JSON.parse(englishJSON);
+
+  t.deepEqual(actual, expected);
+});
+
+test('English: Stringify', t => {
+  const actual = NLF.stringify(JSON.parse(englishJSON));
+  const expected = englishFile;
+
+  t.is(actual.trim(), expected.trim());
+});
+
+test('Farsi: Convert', t => {
+  const expected = farsiFile;
+  const actual = NLF.stringify(NLF.parse(expected));
+
+  t.is(actual.trim(), expected.trim());
+});
+
+test('Farsi: Parse', t => {
+  const actual = NLF.parse(farsiFile);
+  const expected = JSON.parse(farsiJSON);
+
+  t.deepEqual(actual, expected);
+});
+
+test('Farsi: Stringify', t => {
+  const actual = NLF.stringify(JSON.parse(farsiJSON));
+  const expected = farsiFile;
+
+  t.is(actual.trim(), expected.trim());
+});
+
+test('Greek: Convert', t => {
+  const expected = greekFile;
+  const actual = NLF.stringify(NLF.parse(expected), false);
+
+  t.is(actual.trim(), expected.trim());
+});
+
+test('Greek: Parse', t => {
+  const actual = NLF.parse(greekFile);
+  const expected = JSON.parse(greekJSON);
+
+  t.deepEqual(actual, expected);
+});
+
+test('Greek: Stringify', t => {
+  const actual = NLF.stringify(JSON.parse(greekJSON), false);
+  const expected = greekFile;
 
   t.is(actual.trim(), expected.trim());
 });
@@ -54,23 +125,23 @@ test('Japanese: Stringify', t => {
   t.is(actual.trim(), expected.trim());
 });
 
-test('English: Convert', t => {
-  const expected = englishFile;
+test('Thai: Convert', t => {
+  const expected = thaiFile;
   const actual = NLF.stringify(NLF.parse(expected));
 
   t.is(actual.trim(), expected.trim());
 });
 
-test('English: Parse', t => {
-  const actual = NLF.parse(englishFile);
-  const expected = JSON.parse(englishJSON);
+test('Thai: Parse', t => {
+  const actual = NLF.parse(thaiFile);
+  const expected = JSON.parse(thaiJSON);
 
   t.deepEqual(actual, expected);
 });
 
-test('English: Stringify', t => {
-  const actual = NLF.stringify(JSON.parse(englishJSON));
-  const expected = englishFile;
+test('Thai: Stringify', t => {
+  const actual = NLF.stringify(JSON.parse(thaiJSON));
+  const expected = thaiFile;
 
   t.is(actual.trim(), expected.trim());
 });
