5558f25af42dd4b0251695324c30a9944c67199a
Jan T. Sott <git@idleberg.com>
2023-07-09 00:18:24 +0200

    update tests

--- tests/default.mjs
+++ tests/default.mjs
@@ -1,44 +1,45 @@
-// Dependencies
 import { basename, dirname, resolve } from 'node:path';
+import { glob } from 'glob';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
 import * as NLF from '../dist/index.mjs';
 import fs from 'node:fs';
-import glob from 'glob';
 import JSON5 from 'json5';
-import test from 'ava';
 
-const __dirname = resolve(dirname(''));
+const files = await glob(resolve(process.cwd(), 'tests/fixtures/*.nlf'));
 
-glob(resolve(__dirname, 'test/fixtures/*.nlf'), (err, files) => {
-  files.map(file => {
-    const fileDir = dirname(file);
-    const fileBase = basename(file, '.nlf');
+await Promise.all(files.map(file => {
+  const fileDir = dirname(file);
+  const fileBase = basename(file, '.nlf');
 
-    test(`Object: ${basename(file)}`, async t => {
-      const nlfFile = await fs.promises.readFile(file, 'utf8');
-      const jsonPath = resolve(fileDir, fileBase + '.json');
-      const jsonFile = await fs.promises.readFile(jsonPath, 'utf8');
+  test(`Object: ${fileBase}`, async () => {
+    const nlfFile = await fs.promises.readFile(file, 'utf8');
+    const jsonPath = resolve(fileDir, fileBase + '.json');
+    const jsonFile = await fs.promises.readFile(jsonPath, 'utf8');
 
-      const nlfString = NLF.stringify(JSON5.parse(jsonFile));
+    const nlfString = NLF.stringify(JSON5.parse(jsonFile));
 
-      // Remove comments and normalize line endings
-      const actual = nlfString.replace(/^#.*(\r?\n|$)/mg, '').replace(/\r\n/g, '\n');
-      const expected = nlfFile.trim().replace(/^#.*(\r?\n|$)/mg, '').replace(/\r\n/g, '\n');
+    // Remove comments and normalize line endings
+    const actual = nlfString.replace(/^#.*(\r?\n|$)/mg, '').replace(/\r\n/g, '\n');
+    const expected = nlfFile.trim().replace(/^#.*(\r?\n|$)/mg, '').replace(/\r\n/g, '\n');
 
-      t.is(actual, expected);
-    });
-
-    test(`JSON: ${basename(file)}`, async t => {
-      const nlfFile = await fs.promises.readFile(file, 'utf8');
-      const jsonPath = resolve(fileDir, fileBase + '.json');
-      const jsonFile = await fs.promises.readFile(jsonPath, 'utf8');
-
-      const nlfString = NLF.stringify(jsonFile);
-
-      // Remove comments and normalize line endings
-      const actual = nlfString.replace(/^#.*(\r?\n|$)/mg, '').replace(/\r\n/g, '\n');
-      const expected = nlfFile.trim().replace(/^#.*(\r?\n|$)/mg, '').replace(/\r\n/g, '\n');
-
-      t.is(actual, expected);
-    });
+    assert.is(actual, expected);
   });
-});
+
+  test(`JSON: ${fileBase}`, async () => {
+    const nlfFile = await fs.promises.readFile(file, 'utf8');
+    const jsonPath = resolve(fileDir, fileBase + '.json');
+    const jsonFile = await fs.promises.readFile(jsonPath, 'utf8');
+
+    const nlfString = NLF.stringify(jsonFile);
+
+    // Remove comments and normalize line endings
+    const actual = nlfString.replace(/^#.*(\r?\n|$)/mg, '').replace(/\r\n/g, '\n');
+    const expected = nlfFile.trim().replace(/^#.*(\r?\n|$)/mg, '').replace(/\r\n/g, '\n');
+
+    assert.is(actual, expected);
+  });
+
+}));
+
+test.run();
