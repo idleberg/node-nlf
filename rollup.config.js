d1a3f54bb5422371728966bf6daca138bd7d3afc
Jan T. Sott <git@idleberg.com>
2023-08-02 22:46:48 +0200

    format

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,24 @@
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  external: [
-    'json5',
+	external: [
+		'json5',
 		'node:os'
-  ],
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.mjs',
-      format: 'esm',
-      sourcemap: true
-    },
-    {
-      file: 'dist/index.cjs',
-      format: 'cjs',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    typescript()
-  ]
+	],
+	input: 'src/index.ts',
+	output: [
+		{
+			file: 'dist/index.mjs',
+			format: 'esm',
+			sourcemap: true
+		},
+		{
+			file: 'dist/index.cjs',
+			format: 'cjs',
+			sourcemap: true
+		}
+	],
+	plugins: [
+		typescript()
+	]
 };
